74b57d3d57b9fceac6d481cae06a5df4e9b015ca
Vivek Rajput <indvivekrajput@gmail.com>
2022-08-28 00:56:16 +0530

    mens-grooming-data-fetch-from-api

--- urbancompany/src/Pages/Hygienic.jsx
+++ urbancompany/src/Pages/Hygienic.jsx
@@ -127,3 +127,7 @@ export default  function Hygienic(){
     )
  
 }
+
+
+
+
--- urbancompany/src/Pages/MensGrooming.jsx
+++ urbancompany/src/Pages/MensGrooming.jsx
@@ -1,7 +1,58 @@
-
+import { useEffect } from "react"
+import { useState } from "react"
+import axios from "axios"
 export default function MensGrooming(){
 
+    const [data,setData]=useState([])
+
+    useEffect(()=>{
+        axios.get(`https://urbancompanydata.herokuapp.com/api/urbanmensdata`)
+        .then((res)=>{
+            setData(res.data)
+        })
+        .catch((err)=>{console.log(err)})
+        
+    },[])
+       console.log("datamens",data)
     return <div>
-        MensGrooming
+
+        {
+            data?.map((el)=>{
+                return  <div>
+                <div>
+                    <div>
+                            <h1>{el.package}</h1>
+                            <h5>{el.description}</h5>
+                            <h1>*{el.rating}</h1>
+
+                            <div >
+                                 <h1>{el.price}</h1>
+                                 <h1>{el.time}</h1>           
+                            </div>
+
+                        <div>
+                            <button>{el.button}</button>
+                        </div>
+                    </div>
+                    <hr />
+                    <div>
+                        <ul>
+                            <li>{el.feature}</li>
+                            <li>{el.benefits}</li>
+                        </ul>
+    
+                    </div>
+                    
+                </div>
+    
+                <div>
+                        <img src={el.image}  />
+                        <button>{el.addbutton}</button>
+                </div>
+             </div>
+            })
+        }
+        
+
     </div>
 }
