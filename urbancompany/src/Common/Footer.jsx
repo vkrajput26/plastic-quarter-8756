1b663b012d9ce3782b683c33269159d6eb888399
Vivek Rajput <indvivekrajput@gmail.com>
2022-08-28 18:22:47 +0530

    creating footer

--- urbancompany/src/Common/Footer.jsx
+++ urbancompany/src/Common/Footer.jsx
@@ -1,5 +1,8 @@
-import {Box} from "@chakra-ui/react"
+
  import {Link} from "react-router-dom"
+ import "../Style/Footer.css"
+ import {Box,Button,SimpleGrid,List,Image,Text,Heading,Select,Input,Stack,InputGroup,InputLeftAddon} from "@chakra-ui/react"
+
 export default function Footer(){
     let style={marginBottom:"25px", display:"flex" ,
     gap:"2rem", marginLeft:"4rem"  }
@@ -7,7 +10,7 @@ export default function Footer(){
 
     return(
         <>
-        <div style={{background:"black", color:"white" ,height:"200px"}}>
+        <div style={{background:"black", color:"white" }}>
             <ul style={style}>
                 <Link to="#">About Us</Link>
                 <li>UC Impact</li>
@@ -27,15 +30,56 @@ export default function Footer(){
             </ul>
             <hr />
 
-            <div>
+            <div style={{width:"90%",margin:"auto"}}>
                 <h4 style={{marginBottom:"25px"}}>Serving In</h4>
                 <span >Aus</span>
               <ul style={style}>
                 <Link to="#">Melbourne</Link>
                 <li>Sydney</li>
               </ul>
+
+              <span >IND</span>
+              <ul className="footerList" >
+                <Link to="#">Agra</Link>
+                <li>Ahmedabad</li>
+                <li>Amritsar</li>
+                <li>Bangalore</li>
+                <li>Bhopal</li>
+                <li>Chennai</li>
+                <li>Cuttack</li>
+                <li>Delhi NCR</li>
+                <li>Gwalior</li>
+                <li>Jabalpur</li>
+                <li>Jaipur</li>
+                <li>Jodhpur</li>
+                <li>Kochi</li>
+                <li>Kota</li>
+                <li>Mumbai</li>
+                <li>Nagpur</li>
+                <li>Surat</li>
+                <li>Varanasi</li>
+                <li>Vijayawada</li>
+                <li>Warangal</li>
+
+
+              </ul>
             </div>
+
+          <br />
+        <hr />
+        <Box display="flex" justifyContent="space-around">
+
+            <Box display="flex">
+                <Image w="240px" h="150px" src="https://thetechportal.com/wp-content/uploads/2020/01/Screenshot_20200130-145621.png" alt="img" />
+                <Text mt="64px">2014-22 UrbanClap Technologies India Pvt Ltd.</Text>
+            </Box>
+                 <Box display="flex" mt="5%" gap="2rem">
+                            <Image src="https://images.urbanclap.com/image/upload/categories/category_v2/category_7f907eb0.png" alt="img" w="140px" h="50px" />
+                            <Image  src="https://images.urbanclap.com/image/upload/categories/category_v2/category_7f741d10.png" alt="img" w="140px" h="50px" />
+                         </Box>
+     </Box>
         </div>
+
         </>
     )
 }
